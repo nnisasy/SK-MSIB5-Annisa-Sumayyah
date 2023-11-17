66868a3bb7e01cdc05f072d50195f264050c2046
annisa <anisasumayyah26@gmail.com>
2023-11-17 17:45:51 +0700

    pisahin sidebar

--- admin/tambah.php
+++ admin/tambah.php
@@ -7,24 +7,7 @@ include 'main.php';
     <div class="row">
 
         <!-- SIDEBAR START -->
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <div class="position-sticky pt-3 sidebar-sticky">
-                <div class="align-items-center bg-light text-white">
-                    <img class="img-account-profile rounded-circle mb-2" style="display:block; margin:auto;" src="assets/img/logo/logo1.png" style="text-align: center" width="150">
-                    <h6 class="text-center pb-3" style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
-                        <span class="badge bg-secondary">Administrator</span>
-                    </h6>
-                </div>
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php" style="font-family: Verdana, Geneva, Tahoma, sans-serif;font-size: 14px;">
-                            <i class="bi-cart-fill"></i>
-                            <b>Produk</b>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <?php include 'sidebar.php' ?>
         <!-- SIDEBAR END -->
 
         <!-- CONTENT START -->
@@ -116,7 +99,7 @@ include 'main.php';
                             <a href="index.php" class="btn btn-danger">Kembali</a>
                         </div>
                     </form>
-                    <!-- FORM CREATE DATA START -->
+                    <!-- FORM CREATE DATA END -->
 
                 </div>
             </div>
